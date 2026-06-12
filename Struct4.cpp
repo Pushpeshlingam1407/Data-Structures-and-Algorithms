e8f9c7a408a41953851b6334beecaa16797d7e33
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-12 12:21:14 +0530

    Add implementation of a student structure with nested address structure

---
+++ Struct4.cpp
@@ -0,0 +1,28 @@
+//* Struture with in the structure
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct student
+{
+    int code;
+    string name;
+    string desig;
+    struct
+    {
+        string area;
+        string city;
+        string state;
+    } add;
+} emp = {
+    100,
+    "Pushpesh",
+    "DSA",
+    {"MM Road",
+     "Ongole",
+     "AP"}};
+int main()
+{
+    cout << emp.code << " " << emp.name << " " << emp.desig << "\n";
+    cout << emp.add.area << " " << emp.add.city << " " << emp.add.state << "\n";
+}
