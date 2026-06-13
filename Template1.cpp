f3ac93dba193c4e2b4cde94e5ce757f4a2bc66c5
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-13 15:14:22 +0530

    feat: add class template implementation for basic arithmetic operations

---
+++ Template1.cpp
@@ -0,0 +1,45 @@
+// Program demonstrating class template for basic arithmetic operations (add and subtract)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+class Arithmetic{
+private:
+    T a;
+    T b;
+public:
+    Arithmetic(T a, T b);
+    T add();
+    T sub();
+};
+
+template <class T> // Must declare template<class T> for each member function definition outside the class scope
+Arithmetic <T>:: Arithmetic(T a,T b){
+    this->a=a;
+    this->b=b;
+}
+
+template<class T>
+T Arithmetic<T>::add(){
+    T c;
+    c=a+b;
+    return(c);
+}
+
+template<class T>
+T Arithmetic<T>::sub(){
+    T c;
+    c=a-b;
+    return(c);
+}
+
+int main() {
+    Arithmetic<int> ar1(10,5);
+    cout<<"Addition of int values: "<<ar1.add()<<endl;
+    cout<<"Subtraction of int values: "<<ar1.sub()<<endl;
+
+    Arithmetic<float> ar2(13.5,13.8);
+    cout<<"Addition of float values: "<<ar2.add()<<endl;
+    cout<<"Subtraction of float values: "<<ar2.sub()<<endl;
+}
