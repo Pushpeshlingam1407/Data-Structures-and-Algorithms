5ccdade6f03c72531aaea7d0bd82b4cdecc2e47f
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-13 13:55:18 +0530

    Add implementation for array input/output using pointers in Pointer3.cpp

---
+++ Pointer3.cpp
@@ -0,0 +1,21 @@
+//* Array input/output using pointers, printing values and their addresses.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    int *p = a;
+
+    for (int i = 0; i < n; i++)
+        cin >> *(p + i);
+
+    for (int i = 0; i < n; i++)
+        cout << *(p + i) << "\t" << (p + i) << "\n";
+
+    return 0;
+}
