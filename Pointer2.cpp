a041aaeed812b7ce746197dc46e036e851039794
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-13 13:43:17 +0530

    Add initial implementation of structures with pointers in Pointer2.cpp

---
+++ Pointer2.cpp
@@ -0,0 +1,18 @@
+
+//* Using a pointer to access structure members
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    struct
+    {
+
+        int code;
+        char name[20];
+        char city[20];
+    } *emp;
+    cin >> emp->code >> emp->name >> emp->city;
+    cout << "Code: " << emp->code << endl;
+    cout << "Name: " << emp->name << endl;
+    cout << "City: " << emp->city << endl;
+}
