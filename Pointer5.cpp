d1b3b2a26d5cacb34a10d374d6b6003d28a10dbc
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-13 14:41:32 +0530

    feat: add examples for structure reference aliasing and 2D array pointer traversal

---
+++ Pointer5.cpp
@@ -0,0 +1,23 @@
+//* variable references 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[3][3], r = 3, c = 3;
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> *(*(a + i) + j);
+        }
+    }
+
+    cout << "\nValue\tAddress\n";
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cout << *(*(a + i) + j) << "\t" << (*(a + i) + j) << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ References1.cpp
@@ -0,0 +1,34 @@
+//* Demonstrates structure usage with reference aliasing and string copy operations in C-style character arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct student{
+    int code;
+    char name[10];
+    char city[10];
+};
+
+int main(){
+    struct student st1;
+    struct student &s = st1;
+
+    cout << sizeof(st1) << endl;
+    cout << sizeof(s) << endl;
+
+    st1.code = 100;
+    strcpy(st1.name, "Pushpesh");
+    strcpy(st1.city, "Ongole");
+
+    cout << st1.code << endl << st1.name << endl << st1.city << endl;
+    cout << s.code << endl << s.name << endl << s.city << endl;
+
+    s.code = 200;
+    strcpy(s.name, "David");
+    strcpy(s.city, "Hyd");
+
+    cout << st1.code << endl << st1.name << endl << st1.city << endl;
+    cout << s.code << endl << s.name << endl << s.city << endl;
+
+    return 0;
+}
