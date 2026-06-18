e86285d394c6491f79fcaa7e0d7df301e5be71df
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-18 11:54:43 +0530

    feat: implement linked list creation and display functionality in Single_LL.cpp

--- Single_LL.cpp
+++ Single_LL.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
+struct Node {
   int data;
   struct Node *next;
 };
-struct Node *head=nullptr;
+struct Node *head = nullptr;
 
-void create(){
+void create() {
   struct Node *newNode, *temp;
-  
+  int n, i;
+  cout << "Enter the Number of Nodes:";
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    cout << "Enter Elements: ";
+    cin >> newNode->data;
+    newNode->next = nullptr; // Assign Null Initially
+    if (head == nullptr) {
+      head = newNode;
+      temp = newNode;
+    } else {
+      temp->next = newNode;
+      temp = newNode;
+    }
+  }
+}
+void display() {
+  struct Node *temp;
+  if (head == nullptr) {
+    cout << "List is Empty!";
+    return;
+  }
+  temp = head;
+  while (temp != nullptr) {
+    cout << "->" << temp->data;
+    temp = temp->next;
+  }
+  cout << "->null";
 }
