bf58513ba80fb2fb04a1aad3c9d72964e8b8b61c
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-19 16:13:24 +0530

    feat: add student struct and implementation for a better array approach

--- Real_Array.cpp
+++ Real_Array.cpp
@@ -15,6 +15,8 @@ Implement using a Array in C++.
 */
 
 //* Brute Force Approach!!
+
+/*
 #include <iostream>
 using namespace std;
 
@@ -141,5 +143,70 @@ int main() {
   cout << "Enter Code to Delete: ";
   remove(arr, n);
   cout << endl;
+  display(arr, n);
+}
+
+*/
+
+//? Better approach
+#include <iostream>
+using namespace std;
+
+struct Student {
+  int code;
+  string name;
+  string course;
+  string Doj;
+  float fees;
+};
+void insert(Student arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << "\nEnter Code of Student " << i + 1 << ": ";
+    cin >> arr[i].code;
+    cout << "Enter Name of Student " << i + 1 << ": ";
+    cin >> arr[i].name;
+    cout << "Enter Course of Student " << i + 1 << ": ";
+    cin >> arr[i].course;
+    cout << "Enter Date of Joining of Student " << i + 1 << ": ";
+    cin >> arr[i].Doj;
+    cout << "Enter Fees of Student " << i + 1 << ": ";
+    cin >> arr[i].fees;
+  }
+}
+
+void printAll(Student arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << "\nCode: " << arr[i].code << endl;
+    cout << "Name: " << arr[i].name << endl;
+    cout << "Course: " << arr[i].course << endl;
+    cout << "Date of Joining: " << arr[i].Doj << endl;
+    cout << "Fees: " << arr[i].fees << endl;
+  }
+}
+void display(Student arr[], int n) {
+  if (n == 0) {
+    cout << "No Records Found!\n";
+    return;
+  }
+  printAll(arr, n);
+}
+// void update(Student arr[], int n){
+
+// }
+// void remove(Student arr[], int n){
+
+// }
+// void search(Student arr[], int n){
+
+// }
+// void displayOne(Student arr[], int n){
+
+// }
+int main() {
+  int n;
+  cout << "Enter Number of Students: ";
+  cin >> n;
+  Student arr[n];
+  insert(arr, n);
   display(arr, n);
 }
