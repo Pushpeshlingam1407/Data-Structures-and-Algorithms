966b442de621fe84b03a40b60434021fa1f338ce
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-20 17:51:23 +0530

    feat: add template file for stack implementation and concepts

---
+++ Stack1.cpp
@@ -0,0 +1,8 @@
+//* STACKS
+
+//* Stack Underflow - The case when stack is empty and we try to pop or peek from it.
+//* Stack Overflow - The case when stack is full and we try to push an element into it.
+//? In Stack, all the operations are performed on the top of the stack.
+
+#include <iostream>
+using namespace std;
