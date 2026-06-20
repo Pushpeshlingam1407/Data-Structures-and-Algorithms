05e60deb8d32bd10727f89288bf6b207ea33924f
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-20 17:11:45 +0530

    refactor: rename Node struct to Student and implement core creation, display, and memory cleanup functions

--- Real_LL.cpp
+++ Real_LL.cpp
@@ -1,48 +1,95 @@
-/*
-=========================================================
-        STUDENT MANAGEMENT SYSTEM USING LINKED LIST
-=========================================================
-
-Objective:
-Implement a Student Management System using a Singly
-Linked List to store and manage student records.
-
-Student Information:
-- Student Code
-- Student Name
-- Course
-- Date of Joining
-- Fees
-
-Features:
-1. Insert a new student record
-2. Update an existing student record
-3. Delete a student record
-4. Search for a student by code
-5. Display a specific student record
-6. Display all student records
-
-Note:
-- Each node stores one student record.
-- Student Code is used as a unique identifier.
-- All operations must be performed using linked list traversal.
-
-=========================================================
-*/
-
 #include <iostream>
+#include <string>
 using namespace std;
 
-struct Node {
+struct Student {
   int code;
   string name;
   string course;
   string doj;
   float fees;
-  struct Node *next;
+  Student *next;
 };
-struct Node *head = nullptr;
 
-void create(){
-    
+Student *head = nullptr;
+
+void create() {
+  Student *newStudent;
+  Student *temp = nullptr;
+  int n;
+
+  cout << "Enter the Number of Students: ";
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    newStudent = new Student;
+
+    cout << "\nStudent " << i << endl;
+
+    cout << "Enter Code   : ";
+    cin >> newStudent->code;
+
+    cin.ignore();
+
+    cout << "Enter Name   : ";
+    getline(cin, newStudent->name);
+
+    cout << "Enter Course : ";
+    getline(cin, newStudent->course);
+
+    cout << "Enter DOJ    : ";
+    getline(cin, newStudent->doj);
+
+    cout << "Enter Fees   : ";
+    cin >> newStudent->fees;
+
+    newStudent->next = nullptr;
+
+    if (head == nullptr) {
+      head = newStudent;
+      temp = newStudent;
+    } else {
+      temp->next = newStudent;
+      temp = newStudent;
+    }
+  }
+}
+
+void display() {
+  if (head == nullptr) {
+    cout << "\nList is Empty!\n";
+    return;
+  }
+
+  Student *temp = head;
+
+  cout << "\nStudent Records:\n\n";
+
+  while (temp != nullptr) {
+    cout << "[" << temp->code << " | " << temp->name << " | " << temp->course
+         << " | " << temp->doj << " | " << temp->fees << "] -> ";
+
+    temp = temp->next;
+  }
+
+  cout << "NULL\n";
+}
+
+void freeList() {
+  Student *temp;
+
+  while (head != nullptr) {
+    temp = head;
+    head = head->next;
+    delete temp;
+  }
+}
+
+int main() {
+  create();
+
+  cout << "\nOriginal List:\n";
+  display();
+
+  freeList();
 }
