d9f453b9f1525c7e64a65ac20f3305e476e2d1d2
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-22 23:03:25 +0530

    feat: add inorder traversal function and user prompts for tree construction

--- Trees1.cpp
+++ Trees1.cpp
@@ -25,21 +25,29 @@ Node *insert(Node *root, int val) {
   } else if (val > root->data) {
     root->right = insert(root->right, val);
   }
-
   return root;
+}
+void inorder(Node *root) {
+  if (root != nullptr) {
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+  }
 }
 
 int main() {
   Node *root = nullptr;
 
   int size;
+  cout << "Enter the size of the tree: ";
   cin >> size;
 
   for (int i = 0; i < size; i++) {
     int val;
+    cout << "Enter the value: ";
     cin >> val;
     root = insert(root, val);
   }
-
-  return 0;
+  cout << "Inorder traversal: ";
+  inorder(root);
 }
