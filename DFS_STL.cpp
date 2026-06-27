a2b7df455911aafad7d8bb67e04aec64d621c3fa
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-27 23:40:39 +0530

    feat: implement depth-first search traversal algorithm using STL components

---
+++ DFS_STL.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SIZE 5
+
+int graph[SIZE][SIZE] = {{0, 1, 1, 0, 0},
+                         {1, 0, 0, 1, 0},
+                         {1, 0, 0, 0, 1},
+                         {0, 1, 0, 0, 1},
+                         {0, 0, 1, 1, 0}};
+
+bool visited[SIZE] = {false};
+
+void DFS(int node) {
+  visited[node] = true;
+  cout << node << " ";
+
+  for (int i = 0; i < SIZE; i++) {
+    if (graph[node][i] == 1 && !visited[i]) {
+      DFS(i);
+    }
+  }
+}
+
+int main() {
+  cout << "DFS Traversal: ";
+  DFS(0);
+  cout << endl;
+  return 0;
+}
