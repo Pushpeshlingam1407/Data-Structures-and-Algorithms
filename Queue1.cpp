cab8a4eab08b758dddc7ddcf95f86a5c23da621e
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-27 22:59:18 +0530

    feat: implement dequeue, display functions and main execution logic for queue operations

--- Queue1.cpp
+++ Queue1.cpp
@@ -6,20 +6,45 @@ using namespace std;
 int queue[SIZE];
 int front = -1, rear = -1;
 
-void enqueue(int val)
-{
-    if (rear == SIZE - 1)
-        cout << "Stack Overflow" << endl;
-    else
-    {
-        if (front == -1)
-            front = 0;
-        rear++;
-        queue[rear] == val;
-        cout<<val<<" Inserted"<<endl;
-    }
+void enqueue(int val) {
+  if (rear == SIZE - 1)
+    cout << "Stack Overflow" << endl;
+  else {
+    if (front == -1)
+      front = 0;
+    rear++;
+    queue[rear] == val;
+    cout << val << " Inserted" << endl;
+  }
 }
 
-void dequeue(){
-    if(front==-1 || front>rear) cout<<"Queue Underflow"<<endl;
+void dequeue() {
+  if (front == -1 || front > rear)
+    cout << "Queue Underflow" << endl;
+  else {
+    cout << "Deleted Element = " << queue[front] << endl;
+    front++;
+  }
+}
+
+void display() {
+  if (front == -1 || front > rear)
+    cout << "Queue is Empty" << endl;
+  else {
+    cout << "Queue Elements: ";
+    for (int i = front; i <= rear; i++) {
+      cout << queue[i] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  enqueue(10);
+  enqueue(20);
+  enqueue(30);
+  enqueue(40);
+  enqueue(50);
+
+  display();
 }
