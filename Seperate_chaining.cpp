b09bc1831723ec11876d9052ce2df9b44429a7c7
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-29 18:49:31 +0530

    docs: update time complexity documentation for separate chaining to include worst-case scenarios

--- Seperate_chaining.cpp
+++ Seperate_chaining.cpp
@@ -1,15 +1,15 @@
 // TODO: Implemention of Separate Chaining
-//? A separate chaining is a Collision Resolution Technique used in Hash Tables
-//? It is a method of storing multiple values in the same index of the hash table 
-//? The values are stored in a linked list at the index
+// ? A separate chaining is a Collision Resolution Technique used in Hash Tables
+// ? It is a method of storing multiple values in the same index of the hash table 
+// ? The values are stored in a linked list at the index
 
 //! Formula
 //? h(key) = key % SIZE
 
 //! Time Complexity:
-//! Insert: O(1) Average
-//! Search: O(1) Average
-//! Delete: O(1) Average
+//! Insert: O(1) Average, O(n) worst case
+//! Search: O(1) Average, O(n) worst case
+//! Delete: O(1) Average, O(n) worst case
 //! Space Complexity: O(n)
 
 #include <iostream>
