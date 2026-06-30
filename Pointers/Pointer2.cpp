0c0da838fed86ae2f0fa7f837fb073a2ff0cffe8
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-30 22:35:11 +0530

    feat: add program demonstrating structure member access via pointers using arrow operator

--- Pointers/Pointer2.cpp
+++ Pointers/Pointer2.cpp
@@ -1,17 +1,15 @@
-//TODO: Using a pointer to access and display structure members (arrow operator ->)
+// TODO: Using a pointer to access and display structure members (arrow operator
+
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    struct
-    {
-
-        int code;
-        char name[20];
-        char city[20];
-    } *emp;
-    cin >> emp->code >> emp->name >> emp->city;
-    cout << "Code: " << emp->code << endl;
-    cout << "Name: " << emp->name << endl;
-    cout << "City: " << emp->city << endl;
+int main() {
+  struct {
+    int code;
+    char name[20];
+    char city[20];
+  } *emp;
+  cin >> emp->code >> emp->name >> emp->city;
+  cout << "Code: " << emp->code << endl;
+  cout << "Name: " << emp->name << endl;
+  cout << "City: " << emp->city << endl;
 }
